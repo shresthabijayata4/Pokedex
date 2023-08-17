ec054d9538deef394a18e869bbe70b9a10afd216
Bijayata <user@users-MacBook-Pro.local>
2023-08-17 22:17:15 +0545

    Add logic to the add on the list

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,51 +1,59 @@
-  import React from "react";
-  import Button from "react-bootstrap/Button";
-  import Modal from "react-bootstrap/Modal";
-  import { useState } from "react";
-  import { BsStar } from "react-icons/bs";
-  import { FaStar } from "react-icons/fa";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import { BsStar } from "react-icons/bs";
+import { FaStar } from "react-icons/fa";
 
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-  const Card = ({ pokemon, loading, infoPokemon, pokemonList }) => {
-    // console.log(pokemon);
-    const [favourite, setFavourite] = useState(false);
+const Card = ({ pokemon, loading, infoPokemon, pokemonList }) => {
+  // console.log(pokemon);
+  const [favourite, setFavourite] = useState(false);
+  const [selectedId, setSelectedId] = useState([]);
 
-    return (
-      <>
-        <div className="container">
-          <div className="row">
-            {loading ? (
-              <h1>Loading...</h1>
-            ) : (
-              pokemon.map((item) => {
-                return (
-                  <>
-                    <div className="col-md-3">
-                      <div className=" card w-[auto] m-4" key={item.id}>
-                        <div className="d-flex justify-end pr-3 pt-2 text-xl">
-                          {!favourite ? (
-                            <BsStar
-                              onClick={() => {
-                                setFavourite(true);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            pokemon.map((item) => {
+              return (
+                <>
+                  <div className="col-md-3">
+                    <div className=" card w-[auto] m-4" key={item.id}>
+                      <div className="d-flex justify-end pr-3 pt-2 text-xl">
+                        {!selectedId.filter((itemId) => itemId === item.id)
+                          .length > 0 ? (
+                          <BsStar
+                            onClick={() => {
+                              // setFavourite(true);
+                              selectedId.push(item.id);
 
-                                pokemonList.dispatch(
-                                  pokemonList.addPoke({
-                                    image: item.sprites.front_default,
-                                    name: item.name,
-                                    id: item.id,
-                                  })
-                                );
-                              }}
-                            />
-                          ) : (
-                            <FaStar
-                              onClick={() => setFavourite(false)}
-                              className="text-yellow-500"
-                            />
-                          )}
+                              pokemonList.dispatch(
+                                pokemonList.addPoke({
+                                  image: item.sprites.front_default,
+                                  name: item.name,
+                                  id: item.id,
+                                })
+                              );
+                            }}
+                          />
+                        ) : (
+                          <FaStar
+                            onClick={() => {
+                              // setFavourite(false);
+                              setSelectedId((prevstate) =>
+                                prevstate.filter((itemId) => itemId != item.id)
+                              );
+                            }}
+                            className="text-yellow-500"
+                          />
+                        )}
 
-                          {/* {pokemonList.poke.map((pokemonInList) => {
+                        {/* {pokemonList.poke.map((pokemonInList) => {
                           
                             return (
                               <>
@@ -58,27 +66,27 @@
                               </>
                             );
                           })} */}
-                        </div>
-
-                        {/* <AiOutlineStar /> */}
-
-                        <img
-                          src={item.sprites.front_default}
-                          alt=""
-                          className="h-auto w-[150px] content-center"
-                          onClick={() => infoPokemon(item)}
-                        />
-
-                        <h3 className="text-center font-bold	">{item.name}</h3>
                       </div>
+
+                      {/* <AiOutlineStar /> */}
+
+                      <img
+                        src={item.sprites.front_default}
+                        alt=""
+                        className="h-auto w-[150px] content-center"
+                        onClick={() => infoPokemon(item)}
+                      />
+
+                      <h3 className="text-center font-bold	">{item.name}</h3>
                     </div>
-                  </>
-                );
-              })
-            )}
-          </div>
+                  </div>
+                </>
+              );
+            })
+          )}
         </div>
-      </>
-    );
-  };
-  export default Card;
+      </div>
+    </>
+  );
+};
+export default Card;
